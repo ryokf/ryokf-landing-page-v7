a1f307ea822d28cf207d9cbdf4fc55c406468a7c
ryokf <ryokhrisnaf@email.com>
2025-01-13 08:15:15 +0700

    ✨feature: navbar

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Navbar from './section/Navbar';
+const App = () => {
+  return (
+    <main className='max-w-7xl mx-auto'>
+      <Navbar></Navbar>
+    </main>
+  )
+}
+
+export default App
