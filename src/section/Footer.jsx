31c6e6b4940105805d19bb6f7a8290a9a060a9e2
ryokf <ryokhrisnaf@gmail.com>
2025-05-26 11:03:23 +0700

    🐞 fix: update footer

--- src/section/Footer.jsx
+++ src/section/Footer.jsx
@@ -1,20 +1,21 @@
 const Footer = () => {
+    const year = new Date().getFullYear();
     return (
         <footer className="c-space pt-7 pb-3 border-t border-black-300 flex justify-between items-center flex-wrap gap-5">
 
             <div className="flex gap-3">
-                <div className="social-icon">
+                <a href="https://github.com/ryokf" className="social-icon">
                     <img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2" />
-                </div>
-                <div className="social-icon">
-                    <img src="/assets/twitter.svg" alt="twitter" className="w-1/2 h-1/2" />
-                </div>
-                <div className="social-icon">
-                    <img src="/assets/instagram.svg" alt="instagram" className="w-1/2 h-1/2" />
-                </div>
+                </a>
+                <a href="https://medium.com/@ryokhrisnaf" className="social-icon">
+                    <img src="/assets/medium.webp" alt="medium" className="w-1/2 h-1/2" />
+                </a>
+                <a href="https://www.linkedin.com/in/ryokf/" className="social-icon">
+                    <img src="/assets/linkedin.webp" alt="linkedin" className="w-1/2 h-1/2" />
+                </a>
             </div>
 
-            <p className="text-white-500">© 2024 Ryo khrisna fitriawan. All rights reserved.</p>
+            <p className="text-white-500">©{year} Ryo khrisna fitriawan. All rights reserved.</p>
         </footer>
     );
 };
